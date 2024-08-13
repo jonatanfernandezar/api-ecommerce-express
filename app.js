eddb3aa4aae179fb8f1f074d030123038abeb46c
Jonathan <jonatanfernandezar@gmail.com>
2024-08-13 18:21:40 -0500

    Añadí a la ruta principal el archivo html

--- app.js
+++ app.js
@@ -55,6 +55,10 @@ app.use('/api/v1/orders', orderRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 const port = process.env.PORT || 5000;
 const start = async () => {
   try {
